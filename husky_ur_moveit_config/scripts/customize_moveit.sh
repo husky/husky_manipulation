8eb4ec003d84db06abe0adadc9fec998c46fe3b2
Dave Niewinski <dniewinski@clearpathrobotics.com>
2017-12-05 13:58:29 -0500

    Added some scripts to make customization super simple

---
+++ husky_ur_moveit_config/scripts/customize_moveit.sh
@@ -0,0 +1,15 @@
+if [ $# == 1 ]
+then
+  echo "Creating Directory <" $PWD"/"$1 ">"
+  mkdir "$1"
+  cd "$1"
+
+  echo "Copying Single UR Moveit Config"
+  cp -r $(catkin_find husky_ur_moveit_config)/* .
+  echo "Updating Package"
+  grep -rli 'husky_ur_moveit_config' * | xargs -i@ sed -i 's/husky_ur_moveit_config/'$1'/g' @
+  echo "Done"
+
+else
+  echo "USAGE: customize_moveit.sh [new_moveit_package_name]"
+fi
